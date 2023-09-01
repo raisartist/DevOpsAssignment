1f113d21b7651b8b8f04337b5de0205d1a70f8cc
Raya Pastukhova <pastukhw@amazon.com>
2023-09-01 10:53:39 +0100

    Implement date validation in new customer form

--- forms.py
+++ forms.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import (StringField, TextAreaField, DateField)
 from wtforms.validators import InputRequired, Length
+import datetime
 
 class new_customer_form(FlaskForm):
+        
     name = StringField('Customer Name', validators=[InputRequired(), Length(min=3, max=20)])
     location = StringField('Location e.g. London', validators=[InputRequired(), Length(min=2, max=20)])
-    dateJoined = DateField('Date Joined (yyyy-mm-dd)', validators=[InputRequired()])
+    dateJoined = DateField('Date Joined', validators=[InputRequired()])
     useCase = TextAreaField('use Case e.g. To monitor 2sec video fragments', validators=[InputRequired(), Length(max=300)])
+
+
+    def validate_on_submit(self):
+        if (self.dateJoined.data > datetime.date.today()):
+            return False
+        else:
+             return True
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 import sqlite3
-from forms import new_customer_form
+from forms import new_customer_form, DateValidationException
 
 conn = sqlite3.connect('database.db')
 print ("Opened database successfully");
@@ -88,33 +88,33 @@ def update_set_customer(customer_name):
 
 @app.route("/add_customer", methods = ['POST', 'GET'])
 def add_customer():
-   form = new_customer_form(request.form)
-   if request.method == 'POST':
-      try:
-        #  name = request.form['name']
-        #  dateJoined = request.form['dateJoined']
-        #  useCase = request.form['useCase']
-        #  location = request.form['location']
-        name = form.name.data
-        dateJoined = form.dateJoined.data
-        useCase = form.useCase.data
-        location = form.location.data
-        with sqlite3.connect("database.db") as connection:
-                current = connection.cursor()
-                current.execute("INSERT OR IGNORE INTO customers (name,dateJoined,useCase,location) VALUES (?,?,?,?)",(str(name),str(dateJoined),str(useCase),str(location)) )
-                
-                connection.commit()
-                message = "Customer record successfully added"
-      except Exception as error:
-         connection.rollback()
-         message = str(error)
-      
-      finally:
-        connection.close()
-        # flash(message)
-        # return redirect(url_for("customers_database"))
+    form = new_customer_form(request.form)
+    isValid = form.validate_on_submit()
+    if isValid:
+        if request.method == 'POST':
+            try:
+                name = form.name.data
+                dateJoined = form.dateJoined.data
+                useCase = form.useCase.data
+                location = form.location.data
+                with sqlite3.connect("database.db") as connection:
+                        current = connection.cursor()
+                        current.execute("INSERT OR IGNORE INTO customers (name,dateJoined,useCase,location) VALUES (?,?,?,?)",(str(name),str(dateJoined),str(useCase),str(location)) )
+                        
+                        connection.commit()
+                        message = "Customer record successfully added"
+            except Exception as error:
+                connection.rollback()
+                message = str(error)
+            
+            finally:
+                connection.close()
+                # flash(message)
+                # return redirect(url_for("customers_database"))
+                return render_template("result.html",message = message)
+    else:
+        message = "The date must not be in the future."
         return render_template("result.html",message = message)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
